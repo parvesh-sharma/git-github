931834edb00a00f5824ce3396f67aaa02f84c6f0
SPARVESH <SPARVESH@WGC110BZFRDC2.delhi.ford.com>
2018-10-11 23:11:16 +0530

    sonar

--- GitTest/src/HelloWorld.java
+++ GitTest/src/HelloWorld.java
@@ -1,8 +1,7 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Success!!");
+		System.out.println("Hello World Success2!!");
 	}
 	
 	public int returnSum(int i, int j) {
